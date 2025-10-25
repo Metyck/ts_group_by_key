af1792ba2cbc3f868bf76705f3131441466483ae
Matvey Turluk <matychencia.ua@gmail.com>
2025-10-25 17:02:44 +0300

    added task solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -6,22 +6,7 @@ export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   // write code here;
   const result: GroupsMap<T> = {};
 
-  const sorted = [...items].sort((a: T, b: T) => {
-    const vA = a[key];
-    const vB = b[key];
-
-    if (typeof vA === 'number' && typeof vB === 'number') {
-      return vA - vB;
-    }
-
-    if (typeof vA === 'string' && typeof vB === 'string') {
-      return vA.localeCompare(vB);
-    }
-
-    return 0;
-  });
-
-  for (const item of sorted) {
+  for (const item of items) {
     const groupId = String(item[key]);
 
     if (!result[groupId]) {
